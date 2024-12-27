08ecc439a1ec5c40f79c7ae82e5230893661276c
ismellike <glopez11193@gmail.com>
2024-12-27 17:00:15 -0600

    Improve is_valid_distribution query

--- contracts/arena-group/src/query.rs
+++ contracts/arena-group/src/query.rs
@@ -41,11 +41,13 @@ pub fn is_valid_distribution(deps: Deps, addrs: Vec<String>) -> StdResult<bool> 
     }
 
     let addrs = addrs
-        .iter()
-        .map(|x| deps.api.addr_validate(x))
+        .into_iter()
+        .map(|x| deps.api.addr_validate(&x))
         .collect::<StdResult<Vec<_>>>()?;
 
-    Ok(addrs.iter().all(|x| members_map().has(deps.storage, x)))
+    Ok(addrs
+        .into_iter()
+        .all(|x| members_map().has(deps.storage, &x)))
 }
 
 pub fn is_member(deps: Deps, addr: String) -> StdResult<bool> {
